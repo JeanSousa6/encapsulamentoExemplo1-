d1505a991af8ba4fc064f3318bf839f7900df98b
Jean <112350318+JeanSousa6@users.noreply.github.com>
2024-03-15 13:45:43 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+package Udemy.aulas;
+import java.util.Locale;
+import java.util.Scanner;
+import Udemy.aulas.Rectangle;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+            Locale.setDefault(Locale.US);
+
+            Scanner lerDado = new Scanner(System.in);
+            Rectangle rec = new Rectangle();
+
+            System.out.println("Enter rectangle width and height: ");
+            rec.setWidth(lerDado.nextDouble());
+            rec.setHeight(lerDado.nextDouble());
+
+            System.out.println("AREA = " + rec.Area());
+            System.out.println("PERIMETER = " + rec.Perimeter());
+            System.out.println("DIAGONAL = " + rec.Diagonal());
+
+            lerDado.close();
+
+        }
+    }
---
+++ Rectangle.java
@@ -0,0 +1,38 @@
+package Udemy.aulas;
+
+public class Rectangle {
+    private double width;
+    private double height;
+
+
+    public double getWidth(){
+       return width;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public void setWidth(double width){
+        this.width = width;
+    }
+
+    public void setHeight(double height){
+        this.height = height;
+    }
+    public double Area(){
+
+        return width * height;
+
+    }
+
+    public double Perimeter(){
+        return 2 * (width + height);
+    }
+
+
+    public double Diagonal(){
+        return Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+    }
+
+}
