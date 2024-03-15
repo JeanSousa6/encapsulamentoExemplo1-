16bc5bd3bd03e22213c963cd56c9e44ae0c989a9
Jean <112350318+JeanSousa6@users.noreply.github.com>
2024-03-15 13:46:55 -0300

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,17 +1,8 @@
-package Udemy.aulas;
-
 public class Rectangle {
+  
     private double width;
     private double height;
 
-
-    public double getWidth(){
-       return width;
-    }
-
-    public double getHeight(){
-        return height;
-    }
 
     public void setWidth(double width){
         this.width = width;
